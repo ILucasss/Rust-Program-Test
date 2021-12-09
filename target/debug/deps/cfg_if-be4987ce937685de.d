7f252a3403f9090b6c7ef884090aa6c52f64378d
ILucasss <yujunchengxxx@126.com>
2021-12-09 10:16:25 +0800

    create

---
+++ target/debug/deps/cfg_if-be4987ce937685de.d
@@ -0,0 +1,7 @@
+E:\project\RUST\rust_program_test\target\debug\deps\cfg_if-be4987ce937685de.rmeta: C:\Users\Administrator\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+E:\project\RUST\rust_program_test\target\debug\deps\libcfg_if-be4987ce937685de.rlib: C:\Users\Administrator\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+E:\project\RUST\rust_program_test\target\debug\deps\cfg_if-be4987ce937685de.d: C:\Users\Administrator\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\Administrator\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs:
---
+++ target/debug/deps/rust_program_test-6d0824af8cde838d.d
@@ -0,0 +1,5 @@
+E:\project\RUST\rust_program_test\target\debug\deps\rust_program_test-6d0824af8cde838d.exe: src\main.rs
+
+E:\project\RUST\rust_program_test\target\debug\deps\rust_program_test-6d0824af8cde838d.d: src\main.rs
+
+src\main.rs:
