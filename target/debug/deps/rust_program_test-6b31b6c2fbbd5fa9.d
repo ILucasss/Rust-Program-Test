574e13e05be145b9026e4fac3cee38b625b759da
ILucasss <yujunchengxxx@126.com>
2021-12-09 11:31:13 +0800

    DONE: 修改.gitignore

--- target/debug/deps/rust_program_test-6b31b6c2fbbd5fa9.d
+++
@@ -1,6 +0,0 @@
-E:\project\RUST\rust_program_test\target\debug\deps\rust_program_test-6b31b6c2fbbd5fa9.exe: src\main.rs src\list.rs
-
-E:\project\RUST\rust_program_test\target\debug\deps\rust_program_test-6b31b6c2fbbd5fa9.d: src\main.rs src\list.rs
-
-src\main.rs:
-src\list.rs:
--- target/debug/deps/rust_program_test.d
+++
@@ -1,5 +0,0 @@
-E:\project\RUST\rust_program_test\target\debug\deps\rust_program_test.exe: src\main.rs
-
-E:\project\RUST\rust_program_test\target\debug\deps\rust_program_test.d: src\main.rs
-
-src\main.rs:
